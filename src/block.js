a47b9f369a04bec90f672f965cd347f65f9361ad
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-08-12 16:05:17 +0300

    set simple onclick logic

--- src/block.js
+++ src/block.js
@@ -5,16 +5,17 @@ class Block {
     constructor(height, width) {
         this.height = height
         this.width = width
-        this.element = document.createElement('div')
+        this.block = document.createElement('div')
     }
-    setValuesToElement(shiftRight=0, shiftUp=0) {
-        this.element.className = 'block'
-        this.element.style.height = `${this.height}px`
-        this.element.style.width = `${this.width}px`
-        this.element.style.marginLeft = `${shiftRight}px`
-        this.element.style.marginTop = `${shiftUp}px`
+    setValuesToElement(id, shiftRight=0, shiftUp=0) {
+        this.block.className = 'block'
+        this.block.id = id
+        this.block.style.height = `${this.height}px`
+        this.block.style.width = `${this.width}px`
+        this.block.style.marginLeft = `${shiftRight}px`
+        this.block.style.marginTop = `${shiftUp}px`
     }
     getElement() {
-        return this.element
+        return this.block
     }
 }
--- src/map.js
+++ src/map.js
@@ -9,15 +9,24 @@ class Map {
         this.map = document.getElementById('map')
     }
     makeBlocks(blockHeight, blockWidth) {
-        console.log('a')
         for (let i = 0; i < this.height / blockHeight; i++) {
             this.innerFoundation.push([])
             for (let j = 0; j < this.width / blockWidth; j++) {
                 this.innerFoundation[i].push(null)
                 const block = new Block(blockHeight, blockWidth)
-                block.setValuesToElement(j * blockWidth, i * blockHeight)
-                this.map.appendChild(block.getElement())
+                block.setValuesToElement(`${i}${j}`, j * blockWidth, i * blockHeight)
+                const blockElement = block.getElement()
+                blockElement.onclick = this.setOnclickFunction(blockElement)
+                this.map.appendChild(blockElement)
             }
+        }
+    }
+    setOnclickFunction(blockElement) {
+        return () => {
+            const i = blockElement.id.split('').map(value => Number(value))[0]
+            const j = blockElement.id.split('').map(value => Number(value))[1]
+            this.innerFoundation[i][j] = 'user'
+            console.log(this.innerFoundation)
         }
     }
 }
